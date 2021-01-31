ac586e7cdfc1536a3ac8d3d97cf30280a8433ddd
Daniel Sângelo <danielsangelo@hotmail.com>
2021-01-31 16:07:56 -0300

    views and controllers to the models have been created

---
+++ Contoso-University/Contoso-University/Data/SchoolContext.cs
@@ -0,0 +1,26 @@
+﻿using Contoso_University.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contoso_University.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
+
+        /*In this case, DbSet<Enrollment> and DbSet<Course> could be omitted, because Students references Enrollment and this one references Course*/
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            /*
+             The behavior standard is changed so that the tables have their names in the singular
+             */
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
--- Contoso-University/Contoso-University/Startup.cs
+++ Contoso-University/Contoso-University/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Contoso_University.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Contoso_University
 {
@@ -23,6 +25,9 @@ namespace Contoso_University
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SchoolContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllersWithViews();
         }
 
