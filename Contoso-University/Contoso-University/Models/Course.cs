60093515ad1db69c5678d30e9a376f3eaaf8b9d6
Daniel Sângelo <danielsangelo@hotmail.com>
2021-01-31 15:54:12 -0300

    initial models have been created

---
+++ Contoso-University/Contoso-University/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contoso_University.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] //allow to insert the primary key to the course instead of data base to do that
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ Contoso-University/Contoso-University/Models/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Contoso_University.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        //[StringLength(50, MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        //[StringLength(50)]
+        //[Column("FirstName")] //the property FirstMidName will be mapped to database as firstname
+        public string FirstMidName { get; set; }
+
+        [Display(Name = "Enrollment Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy}")]
+        public DateTime EnrollmentDate { get; set; }
+
+
+        /*
+         If the property just have get method, then it isn't mapped to database. I could use [NotMapped] annotation for this purpose.
+         */
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
